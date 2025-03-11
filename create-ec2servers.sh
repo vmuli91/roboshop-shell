314048dd2cd20c5e2e3a85146b2aefd42e48e459
venkateshreddy <venkateshreddy.muli@gmail.com>
2025-03-11 11:24:20 +0530

    script for create ec2 servers

--- create-ec2servers.sh
+++ create-ec2servers.sh
@@ -7,17 +7,17 @@ SECURITY_GROUP_ID=sg-0275e6067c2c2beb3
 DOMAIN_NAME=masterdevops.site
 HOSTED_ZONE_ID=Z0054923J2O4K3M5H15Y
 
-#For mysql or mongodb the instance type should be t3.micro else t2.micro
+# if mysql or mongodb instance_type should be t3.medium , for all others it is t2.micro
 
 for i in $@
-do
+do  
     if [[ $i == "mongodb" || $i == "mysql" ]]
     then
-        INSTANCE_TYPE="T3.medium"
+        INSTANCE_TYPE="t3.medium"
     else
         INSTANCE_TYPE="t2.micro"
     fi
-    echo "creating $i instances"
+    echo "creating $i instance"
     IP_ADDRESS=$(aws ec2 run-instances --image-id $IMAGE_ID  --instance-type $INSTANCE_TYPE --security-group-ids $SECURITY_GROUP_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" | jq -r '.Instances[0].PrivateIpAddress')
     echo "created $i instance: $IP_ADDRESS"
 
